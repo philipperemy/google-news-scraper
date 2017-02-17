a5b9dfe686c48d873c9f0c47598cd0955d48ad7a
philipperemy <premy@reactive.co.jp>
2017-02-17 18:58:11 +0900

    req and read data

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 from mtranslate import translate
+
+from core import retrieve_data_from_links
 
 
 def main():
@@ -12,4 +13,14 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    # links = [('http://www.excite.co.jp/News/column_g/20161115/TokyoFm_QICfvc0b1m.html',
+    #          'コレってアリ!? 私が体験した「飲食店」ドン引きエピソード')]
+    # links = [('http://www.sankei.com/west/news/160610/wst1606100085-n1.html',
+    #          '尾道市の飲食店から出火、２４棟に延焼 ２人けが、１人体調不良に')]
+    # links = [('https://www.travelvoice.jp/20160808-71985',
+    #          'グーグル検索から飲食店予約が可能に、ホットペッパー・食べログ・一休に ...')]
+
+    links = [('http://www.news24.jp/articles/2016/06/27/07333796.html', '車の自動走行に向け課題を議論 警察庁')]
+
+    retrieve_data_from_links(links, '')
