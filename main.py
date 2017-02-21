fb10aca38040fb7f8599b1f75069db67770fd680
philipperemy <premy@reactive.co.jp>
2017-02-21 17:19:20 +0900

    add expressvpn submodule

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from core import run
+from expressvpn import wrapper
 
 if __name__ == '__main__':
     # https://news.google.co.jp
     # https://news.google.com/?output=rss&hl=fr
     # RSS Feed does not work for Japanese language.
     # get_articles('プロクター・アンド・ギャンブル')
-    run()
+
+    while True:
+        try:
+            run()
+        except Exception as e:
+            print('EXCEPTION CAUGHT in __MAIN__')
+            print(e)
+            print('Lets change our PUBLIC IP GUYS!')
+            wrapper.random_connect()
