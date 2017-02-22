e7c2af9def68c5aeea4cf7ad2ee0d315914af8c7
philipperemy <premy@reactive.co.jp>
2017-02-22 10:37:11 +0900

    no vpn here

---
+++ main_no_vpn.py
@@ -0,0 +1,8 @@
+from core import run
+
+if __name__ == '__main__':
+    # https://news.google.co.jp
+    # https://news.google.com/?output=rss&hl=fr
+    # RSS Feed does not work for Japanese language.
+    # get_articles('プロクター・アンド・ギャンブル')
+    run()
