3fb6ba9de70ec3dc6794a92fe16529e217da4fdf
Philippe Remy <premy.enseirb@gmail.com>
2019-11-28 10:13:22 +0900

    remove unused script

--- kill.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-ps aux | grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn} python | grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn} -v "grep python" | awk '{print $2}' | xargs kill -9
